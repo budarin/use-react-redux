fce81e1d520270c66aa1a80287a1837630dcea6a
Vadim Budarin <budarin-vv@yandex.ru>
2020-05-27 14:20:16 +0300

    fix: refactor

--- src/utils/getDispatchedProps.ts
+++ src/utils/getDispatchedProps.ts
@@ -1,15 +1,40 @@
 // @ts-nocheck
-import bindActionCreators from './bindActionCreators';
-
 const emptyObject = {};
 
-const getDispatchedProps = (actions, dispatch, ownProps = emptyObject) => () => {
-    if (typeof actions === 'function') {
-        return actions(ownProps, dispatch);
+const getDispatchedProps = (mapDispatchToProps, dispatch, ownProps = emptyObject) => () => {
+    if (typeof mapDispatchToProps === 'function') {
+        return mapDispatchToProps(ownProps, dispatch);
     }
 
-    // @ts-ignore
-    return bindActionCreators(actions, dispatch);
+    const dispatchPropsKeys = Object.keys(mapDispatchToProps);
+    const len = dispatchPropsKeys.length;
+    const res = Object.create(null);
+
+    const dispatchFunc = (key) => (...anyProps) => {
+        const action = mapDispatchToProps[key](...anyProps);
+
+        if (action && 'type' in action) {
+            return dispatch(action) || action;
+        }
+
+        return action;
+    };
+
+    if (len > 0) {
+        for (let i = 0; i < len; i++) {
+            const key = dispatchPropsKeys[i];
+
+            // @ts-ignore
+            if (typeof mapDispatchToProps[key] !== 'function') {
+                console.warn(`Action ${key} in mapDispatchToProps should be a function!`);
+                continue;
+            }
+
+            res[key] = dispatchFunc(key);
+        }
+    }
+
+    return res;
 };
 
 export default getDispatchedProps;
