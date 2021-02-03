a0ba063248458cc25133f932196a37303b8fba4d
Vadim Budarin <budarin-vv@yandex.ru>
2021-02-03 23:19:08 +0300

    fix: upgrade deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
-import React, { memo, useMemo, useContext, useRef, useLayoutEffect, useEffect } from 'react';
-import { useContextSelection } from 'use-context-selection';
 import ReactDOM from 'react-dom';
+import { useContextSelection } from 'use-context-selection';
+import { memo, useMemo, useContext, useRef, useLayoutEffect, useEffect, useReducer } from 'react';
+
+import type { Context, ClassAttributes } from 'react';
 
 import getDispatchedProps from './utils/getDispatchedProps';
 import { setBatch, getBatch } from './utils/batch';
@@ -17,11 +19,15 @@ const useIsomorphicLayoutEffect = typeof window !== 'undefined' ? useLayoutEffec
 setBatch(ReactDOM.unstable_batchedUpdates);
 
 const createStore = (reducer: Reducer<any>, initState = emptyObject, middlewares = emptyMiddlewaresArray) => {
-    const [state, dispatch] = React.useReducer(reducer, initState);
+    const [state, dispatch] = useReducer(reducer, initState);
     const currentState = useRef(state);
 
     useIsomorphicLayoutEffect(() => {
         currentState.current = state;
+
+        return () => {
+            currentState.current = undefined;
+        };
     });
 
     const store = useMemo(() => {
@@ -44,7 +50,7 @@ const createStore = (reducer: Reducer<any>, initState = emptyObject, middlewares
     return { state, dispatch: store.dispatch };
 };
 
-export const createProvider = (StateContext: React.Context<any>, DispatchContext: React.Context<Dispatch>) => {
+export const createProvider = (StateContext: Context<any>, DispatchContext: Context<Dispatch>) => {
     function StoreProvider({ reducer, initialState = emptyObject, middlewares, children }: IStoreProvider) {
         const { state, dispatch } = createStore(reducer, initialState, middlewares);
 
@@ -58,16 +64,16 @@ export const createProvider = (StateContext: React.Context<any>, DispatchContext
     return memo(StoreProvider);
 };
 
-export const createUseStore = (StateContext: React.Context<any>, DispatchContext: React.Context<Dispatch>) => (
+export const createUseStore = (StateContext: Context<any>, DispatchContext: Context<Dispatch>) => (
     selector = emptySelector,
     actions: IHash<(...args: any[]) => Dispatch> = emptyObject,
-    containerProps: React.ClassAttributes<any> = emptyObject,
+    containerProps: ClassAttributes<any> = emptyObject,
 ) => {
     const dispatch = useContext<Dispatch>(DispatchContext);
     const dispatchProps = useMemo(getDispatchedProps(actions, dispatch, containerProps), [dispatch, actions]);
     const stateProps = useContextSelection(StateContext, (state: any) => selector(state, containerProps));
 
-    return useMemo<React.ClassAttributes<any>>(
+    return useMemo<ClassAttributes<any>>(
         () => ({
             ...containerProps,
             ...stateProps,
@@ -78,7 +84,7 @@ export const createUseStore = (StateContext: React.Context<any>, DispatchContext
     );
 };
 
-export const createStorage = (StateContext: React.Context<any>, DispatchContext: React.Context<Dispatch>) => ({
+export const createStorage = (StateContext: Context<any>, DispatchContext: Context<Dispatch>) => ({
     useStore: createUseStore(StateContext, DispatchContext),
     StoreProvider: createProvider(StateContext, DispatchContext),
 });
